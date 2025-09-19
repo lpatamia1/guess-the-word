8c12c7ae6331572434ab7172363b0a00fda5ddf2
Lily Patamia <77755337+lpatamia1@users.noreply.github.com>
2025-09-19 08:45:21 +0000

    add hangman style

--- game.py
+++ game.py
@@ -1,14 +1,83 @@
 import random
+import string
+
+hangman_stages = [
+'''
+               +---+
+               |   |
+                   |
+                   |
+                   |
+                   |
+             =========''',
+            '''
+               +---+
+               |   |
+               O   |
+                   |
+                   |
+                   |
+             =========''',
+            '''
+               +---+
+               |   |
+               O   |
+               |   |
+                   |
+                   |
+             =========''',
+            '''
+               +---+
+               |   |
+               O   |
+              /|   |
+                   |
+                   |
+             =========''',
+            '''
+               +---+
+               |   |
+               O   |
+              /|\\  |
+                   |
+                   |
+             =========''',
+            '''
+               +---+
+               |   |
+               O   |
+              /|\\  |
+              /    |
+                   |
+             =========''',
+            '''
+               +---+
+               |   |
+               O   |
+              /|\\  |
+              / \\  |
+                   |
+             ========='''
+]
 word_bank = ['python', 'coffee', 'planet', 'window', 'garden', 'music', 'puzzle', 'forest', 'rocket', 'island']
 word = random.choice(word_bank)
 
 guessedWord = ['_'] * len(word)
-attempts = 10
+attempts = 6
+guessed_letters = set()
 
 # Game Loop
 while attempts > 0:
+    print(hangman_stages[6 - attempts])
     print('\nCurrent word: ' + ' '.join(guessedWord))
     guess = input('Guess a letter: ').lower()
+
+    if guess in guessed_letters:
+        print('You already guessed that letter. Try a new one!')
+        continue
+
+    guessed_letters.add(guess)
+
     if guess in word:
         for i in range(len(word)):
             if word[i] == guess:
@@ -18,7 +87,8 @@ while attempts > 0:
         attempts -= 1
         print('Wrong guess! Attempts left: ' + str(attempts))
     if '_' not in guessedWord:
-        print('\Congratulations! You guessed the word: ' + word)
+        print('\nCongratulations! You guessed the word: ' + word)
         break
 if attempts == 0 and '_' in guessedWord:
-    print('\You\'ve run out of attempts! The word was: ' + word)
+    print(hangman_stages[-1])
+    print('\nYou\'ve run out of attempts! The word was: ' + word)
